4348b7491876966939d0f69b7444367f9acebf49
Tuan Nguyen <nvatuan3@gmail.com>
2023-09-04 14:08:58 +0800

    import librowsa

--- meldataset.py
+++ meldataset.py
@@ -4,6 +4,7 @@ import random
 import torch
 import torch.utils.data
 import numpy as np
+import librosa
 from librosa.util import normalize
 from scipy.io.wavfile import read
 from librosa.filters import mel as librosa_mel_fn
