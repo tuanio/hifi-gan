87f0a4f5ba9259b25495880ebf9d8768d22d599c
Tuan Nguyen <nvatuan3@gmail.com>
2023-09-04 13:43:58 +0800

    add checkwavs

--- make_train_valid.py
+++ make_train_valid.py
@@ -1,11 +1,24 @@
 import random
 import glob
 import os
+import librosa
+from tqdm.auto import tqdm
 
 random.seed(1234)
 
+shortest_duration = 1 # 1 second
+
 wav_path = '/home/stud_vantuan/share_with_150/data_CD/train_CD92/wav'
-wavs = os.listdir(wav_path)
+all_wavs = os.listdir(wav_path)
+
+wavs = []
+
+for wav in tqdm(all_wavs, desc="Checking wavs..."):
+    w, sr = librosa.load(wav, sr=None)
+    if w.shape[0] / sr >= shortest_duration:
+        wavs.append(wav)
+
+print("Keep {}%".format(len(wavs) / len(all_wavs)))
 
 random.shuffle(wavs)
 
@@ -14,6 +27,8 @@ train_len = round(len(wavs) * train_ratio)
 
 train_wavs = wavs[:train_len]
 valid_wavs = wavs[train_len:]
+
+
 
 train_name = [i.rsplit('.', 1)[0] + '|' for i in train_wavs]
 valid_name = [i.rsplit('.', 1)[0] + '|' for i in valid_wavs]
