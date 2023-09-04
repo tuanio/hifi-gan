4d0080373b157138734cc3f15c99508eb6cc5be1
Tuan Nguyen <nvatuan3@gmail.com>
2023-09-04 13:32:30 +0800

    fix the script

--- make_train_valid.py
+++ make_train_valid.py
@@ -15,11 +15,11 @@ train_len = round(len(wavs) * train_ratio)
 train_wavs = wavs[:train_len]
 valid_wavs = wavs[train_len:]
 
-train_name = [i.rsplit('.', 1)[0] for i in train_wavs]
-valid_name = [i.rsplit('.', 1)[0] for i in valid_wavs]
+train_name = [i.rsplit('.', 1)[0] + '|' for i in train_wavs]
+valid_name = [i.rsplit('.', 1)[0] + '|' for i in valid_wavs]
 
 with open('CD92/training.txt', 'w') as f:
-    f.write('|\n'.join(train_name))
+    f.write('\n'.join(train_name))
 
 with open('CD92/validation.txt', 'w') as f:
-    f.write('|\n'.join(valid_name))
+    f.write('\n'.join(valid_name))
