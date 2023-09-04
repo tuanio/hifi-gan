264651f628fc06915c53aea1a16b41ffd9df0d23
Tuan Nguyen <nvatuan3@gmail.com>
2023-09-04 13:29:19 +0800

    fix the make script

--- make_train_valid.py
+++ make_train_valid.py
@@ -15,8 +15,11 @@ train_len = round(len(wavs) * train_ratio)
 train_wavs = wavs[:train_len]
 valid_wavs = wavs[train_len:]
 
+train_name = [i.rsplit('.', 1)[0] for i in train_wavs]
+valid_name = [i.rsplit('.', 1)[0] for i in valid_wavs]
+
 with open('CD92/training.txt', 'w') as f:
-    f.write('|\n'.join(train_wavs))
+    f.write('|\n'.join(train_name))
 
 with open('CD92/validation.txt', 'w') as f:
-    f.write('|\n'.join(valid_wavs))
+    f.write('|\n'.join(valid_name))
